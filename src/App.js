1c97b7122cb7c22bee9651c8aac8df834103f95b
sawssen <sawssen.jandoubi10@gmail.com>
2022-06-08 22:25:47 +0200

    adding first application with jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import image from './img_src.jpg';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    return(
+<div className='firstDiv' style={{border:"solid 1px black", maxwidth:"100vw" }}>
+    <h1  className='titlered'>Your name here</h1>
+    <img src={'img_public.jpg'} alt="pic in public"></img>
+    <img src={image} alt="pic in src"></img>
+    <video width={320} height={240} controls>
+     <source src={'/video.mp4'} type="video/mp4" />
+    </video>
+   
+    
+</div>
+        
+    );
+
 }
-
 export default App;
